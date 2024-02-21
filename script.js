3d0fbcd27706c5018e5498454ffc6a64f6b921e5
MuhammadFahrial <rialjhe111@gmail.com>
2024-02-21 22:12:16 +0800

    modified: index.html 	modified: script.js 	modified: style.css

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 // Fetch your API_KEY
-const API_KEY = "AIzaSyDg93EtddNyFtH2lI0h9bkZwG4icQVIMTc";
-const yourAnswer = document.createElement("p");
+const API_KEY = "...";
 
 // Access your API key (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(API_KEY);
@@ -16,26 +15,31 @@ async function run(yourMsg) {
   const result = await model.generateContent(prompt);
   const response = await result.response;
   const text = response.text();
-
   //   console.log(text);
 
-  const profilMessage = document.createElement("h3");
-  const yourMessage = document.createElement("p");
-
-  const answer = document.querySelector(".answer");
+  const div = document.createElement("div");
   const profileAnswer = document.createElement("h3");
   const yourAnswer = document.createElement("p");
-  yourAnswer.classList.add("output");
-  answer.append(profilMessage, yourMessage, profileAnswer, yourAnswer);
-  profilMessage.innerHTML = "You";
-  yourMessage.innerHTML = yourMsg;
+  div.append(profileAnswer, yourAnswer);
+  div.classList.add("your-output");
+  answer.append(div);
   profileAnswer.innerHTML = "Bot AI";
   yourAnswer.innerHTML = text;
 }
 
 const sendMsg = document.getElementById("sendMsg");
 const yourMsg = document.getElementById("inputText");
+const answer = document.querySelector(".answer");
 
 sendMsg.addEventListener("click", () => {
+  const div = document.createElement("div");
+  const profilMessage = document.createElement("h3");
+  const yourMessage = document.createElement("p");
+  div.classList.add("your-message");
+  div.append(profilMessage, yourMessage);
+  answer.append(div);
+  profilMessage.innerHTML = "You";
+  yourMessage.innerHTML = yourMsg.value;
   run(yourMsg.value);
+  yourMsg.value = "";
 });
